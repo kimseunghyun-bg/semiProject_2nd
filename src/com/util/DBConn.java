02ffa189487f8e0d3d652839675d5e10cc0da60e
kimseunghyun.bg <kimseunghyun.bg@gmail.com>
2016-09-13 14:36:15 +0900

    DBconn파일 정보 변경(127.0.0.1 -> 211.238.142.224 / skyblue -> qwerty)

--- src/com/util/DBConn.java
+++ src/com/util/DBConn.java
@@ -12,8 +12,8 @@ public class DBConn {
 	
 	public static Connection getConnection() {
 		// String url="jdbc:oracle:thin:@127.0.0.1:1521:ORCL";
-		String url="jdbc:oracle:thin:@127.0.0.1:1521:XE";
-		String user="skyblue";
+		String url="jdbc:oracle:thin:@211.238.142.224:1521:XE";
+		String user="qwerty";
 		String pwd="java$!";
 		
 		if(conn==null) {
